8b04b3761878ce3f19572cdc99fcc6c2d576a8dc
MareksNo <ulmanismareks@gmail.com>
2024-07-24 10:51:37 +0100

    Add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -24,6 +24,12 @@ const userSchema = new mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
     },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
   },
   { timestamps: true }
 );
